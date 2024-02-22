78a0a9c0775c13603fbd627578dba06aa73437c4
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-02-22 12:20:21 +0500

    code clean

--- test/test.js
+++ test/test.js
@@ -5,16 +5,9 @@ const templateData = {
     name: 'John Doe',
     otp: '03144099558'
 };
-
-// Example recipient email
 const recipientEmail = 'mh408800@gmail.com';
-
-// Example template name
 const templateName = 'otpTemplate';
-
-// Construct the absolute file path to the template file using path module
 const templatePath = path.join(__dirname, '../services/aws SES/otpTemplate.ejs');
-
 (async () => {
     try {
         const result = await sendTemplateEmail(recipientEmail, templatePath, templateData);
